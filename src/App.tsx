d45f2a3a94c1e0ab2c28a01f1de2f9ecc98a661f
adimadur <adityamadur735@gmail.com>
2025-09-28 10:11:45 +0530

    Prompt enhanced

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import "./App.css";
 import Index from "./pages/Landing/Index";
 import ChatPage from "./pages/ChatPage/ChatPage";
 import { ToastProvider } from "./helper/Toast";
-// import Hackathon from "./pages/Hackathon";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -11,28 +10,29 @@ import ProjectGallery from "./components/ProjectGallery";
 import IndividualPost from "./components/HackathonPost";
 import UserDashboard from "./components/UserDashboard";
 import HackathonOverComponent from "./pages/HackathonCompletionPage";
+import Hackathon from "./pages/Hackathon";
 import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
     <>
-     <ToastProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/chatPage/:projectId" element={<ChatPage />} />
-          {/* <Route path="/hackathon" element={<Hackathon />} /> */}
+      <ToastProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/chatPage/:projectId" element={<ChatPage />} />
+            <Route path="/hackathon" element={<HackathonShowcase />} />
 
-          <Route path="/hackathon" element={<HackathonOverComponent/>} />
-          <Route path="/submit" element={<HackathonShowcase />} />
-          <Route path="/gallery" element={<ProjectGallery />} />
-          <Route path="/project/:id" element={<IndividualPost />} />
-          <Route path="/dashboard" element={<UserDashboard />} />
+            {/* <Route path="/hackathon" element={<HackathonOverComponent/>} /> */}
+            <Route path="/submit" element={<HackathonShowcase />} />
+            <Route path="/gallery" element={<ProjectGallery />} />
+            <Route path="/project/:id" element={<IndividualPost />} />
+            <Route path="/dashboard" element={<UserDashboard />} />
 
           {/* Not Found */}
           <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
       </ToastProvider>
     </>
   );
